867dc6b934fa27c10bae8019d3ebf908a8b08d10
dnadendl <dnadendla@uncc.edu>
2016-03-28 22:46:05 +0000

    Done with section 4.x

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -14,8 +14,10 @@ class MoviesController < ApplicationController
   end
   
   # in movies_controller.rb
+
   def create
-    @movie = Movie.create!(params[:movie])
+    #@movie = Movie.create!(params[:movie]) #old way
+    @movie = Movie.create!(movie_params)  # new way
     flash[:notice] = "#{@movie.title} was successfully created."
     redirect_to movies_path
   end
@@ -26,7 +28,8 @@ class MoviesController < ApplicationController
  
   def update
     @movie = Movie.find params[:id]
-    @movie.update_attributes!(params[:movie])
+    #@movie.update_attributes!(params[:movie])  # old way
+    @movie.update_attributes!(movie_params)  # new way  
     flash[:notice] = "#{@movie.title} was successfully updated."
     redirect_to movie_path(@movie)
   end
@@ -36,6 +39,13 @@ class MoviesController < ApplicationController
     @movie.destroy
     flash[:notice] = "Movie '#{@movie.title}' deleted."
     redirect_to movies_path
+  end
+  
+  # add below all other methods
+private
+
+  def movie_params
+    params.require(:movie).permit(:title, :rating, :description, :release_date)
   end
   
 end
