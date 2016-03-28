f4acc572ac36e46550c273e439d63fd275d228f4
dnadendl <dnadendla@uncc.edu>
2016-03-28 22:26:38 +0000

    Finished ch4_wt completely

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,3 +3,9 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 end
+# This file is app/controllers/movies_controller.rb
+class MoviesController < ApplicationController
+  def index
+    @movies = Movie.all
+  end
+end
